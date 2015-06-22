b83cf05c88f712045c8c94ed0a8671e9874ce02b
tonghs <tonghuashuai@gmail.com>
2015-06-22 09:00:59 +0000

    add favicon

--- _route.py
+++ _route.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 #coding:utf-8
 
+import tornado
+from config import STATIC_HOST
+
 class Route(object):
     def __init__(self):
         self.url_list = list()
 
     def __call__(self, url):
         def _(cls):
-            print url, cls
             self.url_list.append((url, cls))
 
             return cls
