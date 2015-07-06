11fbba81afd17a29c0cfb4b000e3d437faffaf29
tonghs <tonghuashuai@gmail.com>
2015-07-06 04:16:43 +0000

    fix

--- misc/coffee_const.py
+++ misc/coffee_const.py
@@ -4,8 +4,14 @@
 
 from mako.template import Template
 from mako.lookup import TemplateLookup
+import sys
+import _env
+
+print sys.path
 
 mylookup = TemplateLookup(directories=['coffee'], output_encoding='utf-8')
 
 mytemplate = mylookup.get_template("const.coffee.mako")
 print(mytemplate.render())
+
+
